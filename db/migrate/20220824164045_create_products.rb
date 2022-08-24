3cfe851dd47f1a2305b0f440ce5d95660f2e3d82
Ale Cofré <ale.cofre.perez@gmail.com>
2022-08-24 13:16:38 -0400

    Creados models,controllers y views con scaffold

---
+++ db/migrate/20220824164045_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :description
+      t.string :condition
+      t.integer :price
+      t.references :brand, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
